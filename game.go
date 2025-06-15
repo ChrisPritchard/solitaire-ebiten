989afb22beb2c41dbf95e4ede4e80431fda26b9e
= <=>
2025-06-15 16:56:04 +1200

    testing some stack formats, fixed texture

--- game.go
+++ game.go
@@ -29,6 +29,22 @@ func (game *Game) Draw(screen *ebiten.Image) {
 		img := assets.Cards[card.Suit][card.Value]
 		screen.DrawImage(img, op)
 	}
+
+	for i := range 15 {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(3, 3)
+		op.GeoM.Translate(400, float64(100-(i*4)))
+		img := assets.CardBack
+		screen.DrawImage(img, op)
+	}
+
+	for i := range 13 {
+		op := &ebiten.DrawImageOptions{}
+		op.GeoM.Scale(3, 3)
+		op.GeoM.Translate(520, float64(100+(i*30)))
+		img := assets.Cards[1][14-i]
+		screen.DrawImage(img, op)
+	}
 }
 
 func (game *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
