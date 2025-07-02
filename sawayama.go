2d1f3039979257b8380f2978c1abc64a148e82be
= <=>
2025-07-02 20:13:41 +1200

    created some shuffling code

--- sawayama.go
+++ sawayama.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"math/rand"
+
 	"github.com/chrispritchard/solitaire-ebiten/assets"
 	"github.com/hajimehoshi/ebiten/v2"
 )
@@ -18,21 +20,56 @@ func (c *StandardDeck) Image() *ebiten.Image {
 	return assets.Cards[c.Suit][c.Value]
 }
 
-type SawayamaRules struct{}
+type gamestate int
 
-func (r *SawayamaRules) Update() error { return nil }
+const (
+	shuffling gamestate = iota
+	dealing
+)
+
+type SawayamaRules struct {
+	cards []Card
+	state gamestate
+}
+
+func (r *SawayamaRules) Update() error {
+	switch r.state {
+	case shuffling:
+		r.cards = shuffle_deck()
+		r.state = dealing
+	}
+	return nil
+}
 
 func (r *SawayamaRules) Cards() []Card {
 
+	return r.cards
+}
+
+func shuffle_deck() []Card {
 	cards := make([]Card, 0)
 	for i := range 4 {
 		for j := range 13 {
 			cards = append(cards, Card{
-				X: i*120 + j*20, Y: j * 20, Z: j,
-				Content: &StandardDeck{Suit: i, Value: j + 2, Visible: j%2 == 0},
+				X: 0, Y: 0, Z: 0,
+				Content: &StandardDeck{Suit: i, Value: j + 2, Visible: false},
 			})
 		}
 	}
 
-	return cards
+	shuffled := make([]Card, 0)
+	for {
+		next := rand.Intn(len(cards))
+		shuffled = append(shuffled, cards[next])
+		if next == 0 {
+			cards = cards[1:]
+		} else {
+			cards = append(cards[:next-1], cards[next+1:]...)
+		}
+		if len(cards) == 0 {
+			break
+		}
+	}
+
+	return shuffled
 }
